1825dc1014ff565a626737e9389a43e109ce2774
umangkachhapati <umangkachhapati12345@gmail.com>
2026-08-15 22:02:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,8 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
 app.use(express.json());
